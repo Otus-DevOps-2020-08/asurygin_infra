8b9c790008c67ec446553d2b5422f3d8dc5ea683
Андрей Сурыгин <a.surigin@gmail.com>
2020-09-23 15:51:44 +0300

    fix install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt install -y apt-transport-https libcurl3-gnutls ca-certificates
+apt-get install -y apt-transport-https libcurl3-gnutls ca-certificates
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 sudo bash -c 'echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.2.list'
 apt-get update
